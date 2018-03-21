99e13239d2fca28f37dd2aa0887a461f16584ad0
Diego Zuluaga <diegozuluaga88@gmail.com>
2018-03-21 17:52:47 -0500

    project-1 sinatra

---
+++ solution.rb
@@ -0,0 +1,12 @@
+require 'sinatra'
+
+get '/' do
+  if params[:nombre].size > 0
+    "<h1>Hola #{params[:nombre]}!</h1>"
+  else
+      "<h1>Hola desconocido!</h1>"
+  end
+end
+
+#nombre =  params[:nombre]  ||   'desconocido'
+#params[:nombre].size > 0 ? "<h1>Hola #{params[:nombre]}!</h1>" : "<h1>Hola desconocido!</h1>"
